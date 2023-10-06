2e653c5f0eb075be46651e12a985cf1d37f786c8
poafs1 <prach.yot@gmail.com>
2023-10-06 18:37:47 +0700

    feat: base repo

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigsModule } from './configs/configs.module';
+import { HealthCheckModule } from './health-check/health-check.module';
+import { SqlModule } from './sql/sql.module';
 
 @Module({
-  imports: [],
+  imports: [ConfigsModule, HealthCheckModule, SqlModule],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/configs/configs.service.ts
@@ -0,0 +1,51 @@
+import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { IJwtConfig } from './interfaces/jwt.config.interface';
+import { IPGConfig } from './interfaces/pg.config.interface';
+import { IServerConfig } from './interfaces/serverConfig.interface';
+
+@Injectable()
+export class ConfigsService {
+  constructor(private readonly config: ConfigService) {}
+
+  get nodeEnv(): string {
+    return this.config.get<string>('NODE_ENV') || 'development';
+  }
+
+  get cookieDomain(): string {
+    return this.config.get<string>('COOKIE_DOMAIN') || 'localhost';
+  }
+
+  get clientUrl(): string {
+    return this.config.get<string>('CLIENT_URL') || 'http://localhost:3000';
+  }
+
+  get serverUrl(): string {
+    return this.config.get<string>('SERVER_URL') || 'http://localhost:8000';
+  }
+
+  get server(): IServerConfig {
+    return {
+      host: this.config.get<string>('HOST_SERVICE') || 'localhost',
+      port: this.config.get<number>('PORT_SERVICE') || 8000,
+    };
+  }
+
+  get pg(): IPGConfig {
+    return {
+      host: this.config.get<string>('PG_HOST') || 'localhost',
+      port: parseInt(this.config.get<string>('PG_PORT')) || 5432,
+      username: this.config.get<string>('PG_USERNAME') || 'postgres',
+      password: this.config.get<string>('PG_PASSWORD') || 'postgres',
+      database: this.config.get<string>('PG_DATABASE') || 'postgres',
+    };
+  }
+
+  get jwt(): IJwtConfig {
+    return {
+      accessTokenLifeTime: this.config.get<string>('JWT_ACCESS_TOKEN_LIFETIME') || '1h',
+      refreshTokenLifeTime: this.config.get<string>('JWT_REFRESH_TOKEN_LIFETIME') || '90d',
+      secret: this.config.get<string>('JWT_SECRET') || 'secret',
+    };
+  }
+}
